726426477cfa235930246efc546b0268d32634d2
LLawlietBLANK <sakshams095@gmail.com>
2025-10-27 00:07:01 +0530

    dataset.py

--- dataset.py
+++ dataset.py
@@ -32,4 +32,58 @@ class BilingualDataset(Dataset):
 
         enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
         dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence is too long")
+        
+
+        #Add sos and eos to the source text
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(encoder_input , dtype = torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] *enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+
+        #Add sos to decoder input
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(decoder_input , dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
     
+
+        #Add sos and eos to the label (what we expect as output from the decoder)
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens , dtype = torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens , dtype = torch.int64)
+            ]
+        )
+
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+
+        return {
+            "encoder_input": encoder_input, #Seq_Len
+            "decoder_input": decoder_input, #Seq_Len
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), #(1,1,Seq_Len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & causal_mask(decoder_input.size(0)), #(1 , Seq_Len) & (1 , Seq_Len , Seq_Len)
+            "label": label, #Seq_Len
+            "src_text": src_text,
+            "tgt_text": tgt_text
+        }
+    
+
+def causal_mask(size):
+    mask = torch.triu(torch.ones(1,size,size),diagonal=1).type(torch.int)
+    return mask == 0
